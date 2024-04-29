208258a532aea4982922cc4256b851a706d48892
Alberto Cangialosi <billyballo.res@gmail.com>
2024-04-29 13:01:19 +0200

    feat(natours): actualGET, POST saving on file, with Structure Formatting JSON

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT | 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the Server Side', app: 'Natours' });
+app.use(express.json());
+
+// Version 0 - starting example
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the Server Side', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint..');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length, // NOTE : not a standard, but useful for counting multiple objects in array
+    data: {
+      tours /* explicitly `tours: tours` */,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint..');
+app.post('/api/v1/tours', (req, res) => {
+  // // DEBUG :
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(PORT, () => {
