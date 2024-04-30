ed9a8f6fe5a24461fe3bee9f4877f95663a49364
Alberto Cangialosi <billyballo.res@gmail.com>
2024-04-30 23:26:50 +0200

    feat(natours): check `NODE_ENV` to use morgan logger
    importing environment variables from from `config.env` with `dotenv` package

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const app = require('./app');
+const dotenv = require('dotenv');
 
+dotenv.config({ path: './config.env' });
 const PORT = process.env.PORT | 3000;
+
+console.log(process.env.PORT);
+
+// console.log(app.get('env'));
+// console.log(process.env);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
