e29127489211f1ad86b451aca6e2f170cefdc7bc
Alberto Cangialosi <billyballo.res@gmail.com>
2024-04-30 16:28:05 +0200

    feat(natours): small demo of serving static file

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   console.log('Hello from the Middleware');
 
