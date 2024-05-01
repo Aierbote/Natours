09ac922cd368a33ab6c418f73259d84f5f7cd40b
Alberto Cangialosi <billyballo.res@gmail.com>
2024-05-01 22:20:57 +0200

    fix(natours): load `config.env` before `require app`

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const app = require('./app');
 const dotenv = require('dotenv');
-
 dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
 const PORT = process.env.PORT | 3000;
 
 console.log(process.env.PORT);
