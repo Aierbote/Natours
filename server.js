f705d2e12ba99f1f4244aa4c90579538a5040971
Alberto Cangialosi <billyballo.res@gmail.com>
2024-05-02 17:21:00 +0200

    feat(natours): add basic mongoose connection

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DATABASE = process.env.DB_STRING;
+const USERNAME = process.env.MY_MONGO_ATLAS_USRNM;
+const PASSWORD = process.env.MY_MONGO_ATLAS_PSSWRD;
+
+const DB = DATABASE.replace('<USERNAME>', USERNAME).replace(
+  '<PASSWORD>',
+  PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    // .connect(process.env.DB_STRING_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB Connection Successful'))
+  .catch((err) =>
+    console.error(
+      `Double check Connection String, either local or remote? ${err}`,
+    ),
+  );
 
 const app = require('./app');
 
