1208c2f0bba0ee9540a2497e9dbf437b77b41c42
Alberto Cangialosi <billyballo.res@gmail.com>
2024-05-03 11:34:58 +0200

    refactor(natours): move tourModel in its own module in models/ folder

--- server.js
+++ server.js
@@ -26,35 +26,6 @@ mongoose
     ),
   );
 
-const tourSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  price: 497,
-  rating: 4.7,
-});
-
-testTour
-  .save()
-  .then((doc) => console.log('doc', doc))
-  .catch((err) => console.error(`🤯 Save Promise rejected ${err}`));
-
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
