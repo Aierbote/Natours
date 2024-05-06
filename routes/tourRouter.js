6bff89a965823f3e4cb7cdede08f14fcfb022561
Alberto Cangialosi <billyballo.res@gmail.com>
2024-05-06 16:30:03 +0200

    feat(natours): add query alias for /top-5-cheaps

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -7,12 +7,14 @@ const {
   getAllTours,
   updateTour,
   deleteTour,
+  aliasTopTours,
 } = require('../controllers/tourControllers');
 
 /* Middlewares */
 
 const router = express.Router();
 
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
