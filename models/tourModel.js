c3ff65a62cf03fd38154f1e9cc474665da583230
Alberto Cangialosi <billyballo.res@gmail.com>
2024-05-07 16:29:36 +0200

    feat(natours): add mongoose document middleware type pre and post
    pre-save-hooks finally adding the slugs!

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -63,6 +65,26 @@ const tourSchema = mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// Document Middleware
+
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+
+  next();
+});
+
+// tourSchema.pre('save', (next) => {
+//   console.log('Saving document...');
+
+//   next();
+// });
+
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
