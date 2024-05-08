22d0d118d63960607828349676c5fcb969288289
Alberto Cangialosi <billyballo.res@gmail.com>
2024-05-08 16:51:30 +0200

    chore(natours): remove 'hello from the Middleware' old example

--- app.js
+++ app.js
@@ -17,13 +17,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the Middleware');
-
-  // IMPORTANT to execute middleware
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
   next();
