d9ee15fcfc3aa7882777d2d0af66ab5e61dad484
Alberto Cangialosi <billyballo.res@gmail.com>
2024-05-13 17:44:30 +0200

    feat(natours): implement `getAllUsers()`

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,13 +1,20 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
 /* Route Handlers */
 
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
     data: {
-      message: 'Not Yet Implemented',
+      users,
     },
   });
-};
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
