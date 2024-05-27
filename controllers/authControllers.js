ff275843ea5102d66c7a265f22de8095ec5ef80d
Alberto Cangialosi <billyballo.res@gmail.com>
2024-05-27 10:32:10 +0200

    fix(natours): remove `new` from  `newPassword` and `newPasswordConfirm`

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -205,8 +205,8 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   }
 
   // 3) If so, update password
-  user.password = req.body.newPassword;
-  user.passwordConfirm = req.body.newPasswordConfirm;
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
 
   await user.save();
   // NOTE : `User.findByIdAndUpdate` will NOT work as intended!
