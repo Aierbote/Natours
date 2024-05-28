3ccf7442eccf042a989482e703e3af63ccd8b11a
Alberto Cangialosi <billyballo.res@gmail.com>
2024-05-28 10:25:00 +0200

    feat(natours): set security http headers with `helmet`
    - adding comments to clarify each middleware in the Middleware Stack

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRouter');
@@ -8,16 +10,31 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-/* Middlewares */
+/* GLOBAL Middlewares */
 
+// Set security HTTP headers
+app.use(helmet()); // here there is a function call to return a middleware
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
