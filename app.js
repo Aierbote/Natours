e104d0d5a01ec1b396701df49ff8127406d6670f
Alberto Cangialosi <billyballo.res@gmail.com>
2024-05-28 11:33:55 +0200

    feat(natours): add data sanitization for nosql injections and xss attacks

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRouter');
@@ -13,7 +15,7 @@ const app = express();
 /* GLOBAL Middlewares */
 
 // Set security HTTP headers
-app.use(helmet()); // here there is a function call to return a middleware
+app.use(helmet());
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
@@ -30,6 +32,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
