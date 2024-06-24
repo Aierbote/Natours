5eb19695943ade9e7d702393a12f2d3d26ceb00e
Alberto Cangialosi <billyballo.res@gmail.com>
2024-06-24 09:50:10 +0200

    feat(natours): preliminary login, redirect reload navbar

--- public/js/login.js
+++ public/js/login.js
@@ -8,8 +8,15 @@ const login = async (email, password) => {
         password,
       },
     });
+
+    if (res.data.status === 'success') {
+      alert('Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
   } catch (err) {
-    console.log(err.response.data.message);
+    alert(err.response.data.message);
   }
 };
 
