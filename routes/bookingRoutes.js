2df13144af4b32f4875ddaf98cb4741f126d7bb3
Alberto Cangialosi <billyballo.res@gmail.com>
2024-07-12 16:21:02 +0200

    feat(natours): finishing the booking API

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -5,24 +5,21 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-// router.use(authController.protect);
+router.use(authController.protect);
 
-router.get(
-  '/checkout-session/:tourId',
-  authController.protect,
-  bookingController.getCheckoutSession,
-);
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
 
-// router
-//   .route('/:id')
-//   .get(reviewController.getReview)
-//   .patch(
-//     authController.restrictTo('user', 'admin'),
-//     reviewController.updateReview,
-//   )
-//   .delete(
-//     authController.restrictTo('user', 'admin'),
-//     reviewController.deleteReview,
-//   );
+router.use(authController.restrictTo('admin', 'lead'));
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
