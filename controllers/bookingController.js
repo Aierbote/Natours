20c423bd1d69fbac3d752f6d2c899bfb4846152c
Alberto Cangialosi <billyballo.res@gmail.com>
2024-07-23 11:12:36 +0200

    fix(deploy): correct stripe `signature` from headers

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -70,10 +70,10 @@ const createBookingCheckout = async (session) => {
 };
 
 exports.webhookCheckout = (req, res, next) => {
-  const signature = req.headers('stript-signature');
-
   let event;
+
   try {
+    const signature = req.headers['stripe-signature'];
     event = stripe.webhooks.constructEvent(
       req.body,
       signature,
